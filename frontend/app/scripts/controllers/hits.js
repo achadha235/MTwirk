8188ee82b9604808ee8fed6ce24a381d767f7879
Salem Hilal <salemhilal@gmail.com>
2014-05-06 03:29:27 -0400

    Front-end auth

--- frontend/app/scripts/controllers/hits.js
+++ frontend/app/scripts/controllers/hits.js
@@ -1,10 +1,15 @@
 'use strict';
 
 angular.module('informant')
-  .controller('HitsCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('HitsCtrl', function ($scope, $location) {
+    $.getJSON("/me", function(data) {
+      console.log(data);
+      if (!data || !data.loggedIn) {
+        console.log("Redirecting...");
+        $scope.$apply(function(){
+          $location.path("/");
+        });
+      }
+    });
+
   });
--- frontend/app/scripts/controllers/main.js
+++ frontend/app/scripts/controllers/main.js
@@ -1,4 +1,11 @@
 'use strict';
 
 angular.module('informant')
-  .controller('MainCtrl', function () { /* Nothing to control, I don't think. */ });
+  .controller('MainCtrl', function ($scope, $location) {
+    $.getJSON('/me', function(data) {
+    if (data && data.loggedIn) {
+      $scope.$apply(function(){
+        $location.path('/hits');
+      });
+    }
+  }); });
